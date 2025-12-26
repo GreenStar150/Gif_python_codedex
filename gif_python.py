07c19581d86c88f90feac13bbb004e3b466f7aef
GreenStar150 <agamdeepsinghdatla2017@gmail.com>
2025-12-26 12:05:09 +0530

    Update gif_python.py

--- gif_python.py
+++ gif_python.py
@@ -1,11 +1,11 @@
 
 import imageio.v3 as iio
-file_names = ['C:\\Users\\agamd\\OneDrive\\Documents\\python codedex proj\\gif\\team-pic1.png',
-              'C:\\Users\\agamd\\OneDrive\\Documents\\python codedex proj\\gif\\team-pic2.png']
+file_names = ['team-pic1.png',
+              'team-pic2.png']
 images = []
 for filename in file_names:
     images.append(iio.imread(filename))
 
 
-iio.imwrite('C:\\Users\\agamd\\OneDrive\\Documents\\python codedex proj\\gif\\team.gif',
-            images, duration=500, loop=0)
+iio.imwrite('team.gif', images, duration=500, loop=0)
+
